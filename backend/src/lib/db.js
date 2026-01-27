b72a6401bde8fc56b1e9b9a4f41c3901ce2a8796
yadunandan28 <yadunandan28704@gmail.com>
2026-01-27 15:59:33 +0530

    sign up section code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    const MONGO_URI = process.env;
+    if(!MONGO_URI) throw new Error("MONGO_URI is not defined in environment variables");
+    
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {

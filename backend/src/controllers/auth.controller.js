12c0cb74223fbf3f50b3c01c20cc11c1675ff844
yadunandan28 <yadunandan28704@gmail.com>
2026-01-29 01:47:19 +0530

    login-refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,6 +68,10 @@ const { fullName, email, password } = req.body;
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
+  if(!email || !password){
+    return res.status(400).json({message: "All fields are required"});
+  }
+
   try {
     const user= await User.findOne({email});
     if(!user){

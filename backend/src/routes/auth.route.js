1fd0f9e49a82b18af3bdc5065af6dcd6e936adb8
yadunandan28 <yadunandan28704@gmail.com>
2026-01-31 00:27:39 +0530

    rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { signup, login,logout,updateProfile} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router=express.Router();
 
+router.use(arcjetProtection);
 router.post('/signup', signup);
-router.post('/login',login);
+router.post('/login',arcjetProtection, login);
 
 router.post('/logout', logout);
 
